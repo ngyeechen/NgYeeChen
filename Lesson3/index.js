f7dcb4bfe82dbe96a40496cbfd2f4cd96b9522f6
ngyeechen <143379473+ngyeechen@users.noreply.github.com>
2025-03-19 22:03:05 +0800

    Lesson3

---
+++ Lesson3/index.js
@@ -0,0 +1,23 @@
+// console.log("asdsfdfag")
+
+// let age =18;
+// let name = "John";
+// let isStudent = true;
+// console.log(age);
+// console.log(name);     
+// console.log(isStudent);
+
+// const hname = "John";
+// const is a constant and it means your hname is John. you cant change it
+
+// var hhno = 123;
+// var is variable is a variable and it means you can change the value of hhno
+// why dont we use var/not recommended? 
+
+// let notkeyword_name = "charlie"
+// let const = "charlie";
+
+// variable names should not have spacing
+uriufe
+weif1212
+dsbs_1
---
+++ Lesson3/script.js
@@ -0,0 +1,22 @@
+// let stockprice = 10;
+// console.log(stockprice);
+// console.log(stockprice+50) // also works if put +50 inside
+
+// update of a variable is redeclaration
+
+// stockprice = STOCKPRICE + 500; //its case sensitive
+
+// stockprice = stockprice ** 2 
+// ** is power
+
+// stockprice = stockprice % 3 //modulus gives remainder
+
+// stockprice *= 3; //equivalent way of stockprice = stockprice + 2 
+
+// stockprice = stockprice * 3
+
+// stockprice = stockprice * 3 ** 2 //same as above *3 and *3 again
+
+// console.log(stockprice);
+
+
